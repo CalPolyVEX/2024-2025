08fd5ffdcfd75638530ad5e49b4715e9c3b193b1
John Seng <jseng@jseng.csc.calpoly.edu>
2018-01-20 17:05:02 -0800

    initial checkin

---
+++ label_me_tools/label_me.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+import sys
+import random
+
+random.seed(101)
+
+if len(sys.argv) <= 1:
+    print "need command line arguments"
+
+files = os.listdir(sys.argv[1])
+#print files
+
+jpg_files = [f for f in files if 'jpg' in f]
+print jpg_files
+num_jpg_files = len(jpg_files)
+
+for x in range(100):
+    rand_name = jpg_files[random.randint(0,num_jpg_files-1)]
+    print rand_name
+
+    #copy file to this directory
+    os.system("cp " + sys.argv[1] + "/" + rand_name + " ./testdir/" + str(x) + ".jpg")
+
+
+    
+    
