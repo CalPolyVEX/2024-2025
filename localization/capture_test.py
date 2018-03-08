8078a145677d61b26a600b9d10501cc897bba551
John Seng <jseng@calpoly.edu>
2018-03-08 17:11:21 +0000

    update

--- localization/capture_test.py
+++ localization/capture_test.py
@@ -1,25 +1,30 @@
 import cv2
-import sys
+import sys, time, datetime
 
 vidcap = cv2.VideoCapture('/dev/video1')
 
 print vidcap
-vidcap.set(3,1280) #capture width
-vidcap.set(4,720) #capture height
-vidcap.set(cv2.CAP_PROP_FPS,30) #capture FPS
+vidcap.set(3,1920) #capture width
+vidcap.set(4,960) #capture height
+vidcap.set(cv2.CAP_PROP_FPS,60) #capture FPS
 
 #vidcap.set(cv2.CAP_PROP_POS_MSEC,20000)      # just cue to 20 sec. position
-for i in range(100):
+for i in range(10):
    success,image = vidcap.read()
-   print success
+   #print success
    if success:
       fps = vidcap.get(cv2.CAP_PROP_FPS)
       height,width,channels = image.shape
       print fps
       print width
       print height
-      res = cv2.resize(image,(width/3, height/3), interpolation = cv2.INTER_AREA)
-      cv2.imwrite("./test_video/frame_ " + str(i) + ".jpg", res)     # save frame as JPEG file
+      start = time.time()
+      res = cv2.resize(image,(width/4, height/4), interpolation = cv2.INTER_AREA)
+      #res = cv2.resize(image,(width/4, height/4))
+      #res = cv2.resize(image,(width, height), interpolation = cv2.INTER_AREA)
+      end = time.time()
+      print (end-start)
+      cv2.imwrite("./test_video/frame_" + str(i) + ".jpg", res)     # save frame as JPEG file
       #cv2.imshow("20sec",image)
       #cv2.waitKey()                    
       #cv2.destroyAllWindows()
--- localization/main.cpp
+++ localization/main.cpp
@@ -58,115 +58,40 @@
 // * \section See3CAM_10CUG
 // * Bayer Camera
 
-
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    qmlRegisterType<Cameraproperty>("econ.camera.property",1,0,"Camproperty");
-    qmlRegisterType<Videostreaming>("econ.camera.stream", 1, 0, "Videostreaming");
-    qmlRegisterType<uvccamera>("econ.camera.uvcsettings", 1, 0, "Uvccamera");
-    qmlRegisterType<UVCExtCx3SNI>("econ.camera.uvcExtsettings", 1, 0, "UvcExtcamera");
-    qmlRegisterType<See3CAM_11CUG>("econ.camera.see3cam11", 1, 0, "See3Cam11");    
-    qmlRegisterType<See3CAM_AR130>("econ.camera.see3camar0130", 1, 0, "See3Camar0130");    
-    qmlRegisterType<See3CAM_10CUG_Bayer>("econ.camera.see3cam10Bayer", 1, 0, "See3Cam10Bayer");
-    qmlRegisterType<See3CAM_10CUG_Mono>("econ.camera.see3cam10Mono", 1, 0, "See3Cam10Mono");
-    qmlRegisterType<See3CAM_80>("econ.camera.see3cam80", 1, 0, "See3Cam80");
-
-  // Added by Sankari : 07 Feb 2017
-    qmlRegisterType<See3CAM_30>("econ.camera.see3cam30", 1, 0, "See3Cam30");
-
- // Added by Sankari: 17 Dec 2016
-    qmlRegisterType<See3CAM_81>("econ.camera.see3cam81", 1, 0, "See3Cam81");
-
-    qmlRegisterType<See3CAM_CU50>("econ.camera.see3cam50", 1, 0, "See3Cam50");    
-    qmlRegisterType<See3CAM_CU130>("econ.camera.see3camcu130", 1, 0, "See3CamCu130");
-
-    // Added by Sankari : 22 Feb 2017
-    qmlRegisterType<See3CAM_CU135>("econ.camera.see3camcu135", 1, 0, "See3CamCu135");
-
-// Added by Sankari: 21 Apr 2017
-    qmlRegisterType<See3CAM_CU40>("econ.camera.see3camcu40", 1, 0, "See3Camcu40");
-
-    qmlRegisterType<See3CAM_130>("econ.camera.see3cam130", 1, 0, "See3Cam130");
-    qmlRegisterType<See3CAM_CU30>("econ.camera.see3camcu30", 1, 0, "See3Camcu30");
-
-    // Added by Sankari: 27 July 2017
-    qmlRegisterType<See3CAM_CU20>("econ.camera.see3camcu20", 1, 0, "See3Camcu20");
-    qmlRegisterType<See3CAM_CU51>("econ.camera.see3cam51", 1, 0, "See3Cam51");
-    qmlRegisterType<ASCELLA>("econ.camera.ascella", 1, 0, "Ascella");
-    qmlRegisterType<See3CAM_Control>("econ.camera.see3camControl", 1, 0, "See3CamCtrl");
-    qmlRegisterType<See3CAM_GPIOControl>("econ.camera.see3camGpioControl", 1, 0, "See3CamGpio");
-    qmlRegisterType<AboutPage>("econ.camera.about", 1, 0, "AboutPage");
-    //Added by Dhurka - 13th Oct 2016
-    /**
-     * @brief qmlRegisterType<CommonEnums> - Access the ENUM list to QML
-     */
-    qmlRegisterType<CommonEnums>("cameraenum",1,0,"CommonEnums");
-
-    QtQuick2ApplicationViewer viewer;
-
     //Create a object for Camera property
     Cameraproperty camProperty;    
 
-    if(argc > 1){
-        if(strcmp(argv[1],"-l") == 0 || strcmp(argv[1],"--log") == 0){
-            Cameraproperty camPropertyParam(true);
-        }
-        else{
-            qDebug()<<"Usage: qtcam [OPTION]";
-            qDebug()<<"-l, --log    to create log in a directory\n";
-            return -1;
-        }
-    }
-
     /* JS */
     if (0 == 0) {
-        QTextStream(stdout) << "test";
-        uvccamera test_cam;
-        test_cam.findEconDevice("");
-        test_cam.initExtensionUnit("");
-        camProperty.checkforDevice();
-        camProperty.setCurrentDevice("0","1");
-        camProperty.openHIDDevice("/dev/video10");
-        std::cout << uvccamera::hid_fd; 
+       QTextStream(stdout) << "test";
+       uvccamera test_cam;
+       See3CAM_CU20 s;
 
-        See3CAM_CU20 s;
-        s.setSensorMode(See3CAM_CU20::SENSOR_HDR_DLO);
-        //s.getSensorMode();
+       camProperty.checkforDevice(); //check for all devices
 
-        exit(0);
+       QString num = "2";
+       QString cu20 = "See3CAM_CU20";
+       camProperty.setCurrentDevice(num, cu20);
+
+       camProperty.openHIDDevice(cu20);
+
+       QTextStream(stdout) << "---set sensor mode: " << s.setSensorMode(See3CAM_CU20::SENSOR_HDR_DLO) << endl;
+       //QTextStream(stdout) << s.setSensorMode(See3CAM_CU20::SENSOR_STANDARD) << endl;
+       if (argc > 1)
+          QTextStream(stdout) << s.setOrientation(false, true) << endl;
+       else
+          QTextStream(stdout) << s.setOrientation(false, false) << endl;
+
+       QTextStream(stdout) << "---get sensor mode: " << s.getSensorMode() << endl;
+
+       //uvccam.exitExtensionUnit();
+       //test_cam.exitExtensionUnit();
+       close(uvccamera::hid_fd);
+       exit(0);
     }
+    return 0;
 
     /* end JS */
-
-    viewer.rootContext()->setContextProperty("camModels", &camProperty.modelCam);
-
-    Videostreaming vs;
-    viewer.rootContext()->setContextProperty("resolutionModel", &vs.resolution);
-    viewer.rootContext()->setContextProperty("stillOutputFormatModel", &vs.stillOutputFormat);
-    viewer.rootContext()->setContextProperty("videoOutputFormatModel", &vs.videoOutputFormat);
-    viewer.rootContext()->setContextProperty("fpsAvailable", &vs.fpsList);
-    viewer.rootContext()->setContextProperty("encodersModel", &vs.encoderList);
-    viewer.rootContext()->setContextProperty("SystemPictureFolder",QStandardPaths::standardLocations(QStandardPaths::PicturesLocation).first());
-    viewer.rootContext()->setContextProperty("SystemVideoFolder",QStandardPaths::standardLocations(QStandardPaths::MoviesLocation).first());
-#if LAUNCHPAD
-    viewer.setMainQmlFile(QStringLiteral("/usr/share/qml/qtcam/Views/qtcam.qml"));
-#else
-    viewer.setMainQmlFile(QStringLiteral("qml/qtcam/Views/qtcam.qml"));
-#endif
-    QObject *rootObject = dynamic_cast<QObject*>(viewer.rootObject());
-
-    QObject::connect(rootObject,SIGNAL(stopCamPreview()),rootObject,SLOT(triggerModeCapture()));
-
-    //Setting the Window ICON
-#if LAUNCHPAD
-    QIcon icon("/usr/share/qml/qtcam/icon/images/icon.jpg");
-#else
-    QIcon icon("qml/qtcam/icon/images/icon.jpg");
-#endif
-    viewer.setIcon(icon);
-    viewer.setTitle("Qtcam");
-    //Bug fix: There's no "close,Restore Down,minimize" button on the top of the QTCAM when restore the application
-    viewer.show();
-    return app.exec();
 }
