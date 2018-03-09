3a28ed28eea297629cfb473293ee7b40ac73a3fc
John Seng <jseng@calpoly.edu>
2018-03-08 22:18:14 -0800

    update

--- localization/opencv_test.py
+++ localization/opencv_test.py
@@ -1,18 +1,20 @@
 import cv2
 import sys
 
-vidcap = cv2.VideoCapture('/home/jseng/www/robot.mpg')
+vidcap = cv2.VideoCapture(sys.argv[1])
 
 print vidcap
 #vidcap.set(cv2.CAP_PROP_POS_MSEC,20000)      # just cue to 20 sec. position
-success,image = vidcap.read()
-print success
-if success:
-   fps = vidcap.get(cv2.CAP_PROP_FPS)
-   height,width,channels = image.shape
-   print fps
-   print width
-   print height
-   cv2.imwrite("./test_video/frame20sec.jpg", image)     # save frame as JPEG file
-    #cv2.imshow("20sec",image)
-    #cv2.waitKey()                    
+
+for x in range(100):
+   success,image = vidcap.read()
+   print success
+   if success:
+      fps = vidcap.get(cv2.CAP_PROP_FPS)
+      height,width,channels = image.shape
+      print fps
+      print width
+      print height
+      cv2.imwrite("./test_video/frame" + format(x, '04d') + ".jpg", image)     # save frame as JPEG file
+      #cv2.imshow("20sec",image)
+      #cv2.waitKey()                    
