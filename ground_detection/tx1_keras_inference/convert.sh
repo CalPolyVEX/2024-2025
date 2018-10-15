dda35cba48e20b3f5a220318c8e3ce13e129d8e2
John Seng <jseng@calpoly.edu>
2018-10-15 05:25:04 +0000

    updated for Keras 2.2

--- ground_detection/tx1_keras_inference/convert.sh
+++ ground_detection/tx1_keras_inference/convert.sh
@@ -1,1 +1,2 @@
-python k2tf_convert.py --model ../gpu_code/saved_models/k_test3.py_model.h5 --numout 48
+#python k2tf_convert.py --model ../gpu_code/saved_models/k_test2.py_model.h5 --numout 48
+python k2tf_convert.py --model ../gpu_code/saved_models/k_test3_mobilenet.py_model.h5 --numout 48
