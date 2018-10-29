4a69b2dfe05003e325b7f030aa5f7fdb19bcf855
John Seng <jseng@calpoly.edu>
2018-10-29 14:19:22 -0700

    update

--- ros/jet_launcher/camera_calibration/solvepnp.py
+++ ros/jet_launcher/camera_calibration/solvepnp.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 
-import cv2, numpy as np
+#max resolution: 740x415
+#(18,45) = (527,339)
+#(27, 27) = (673,409)
+#(9,63) = (424,285)
+#(54,54) = (708,283)
+#(0,36) = (357,394)
 
-objectPoints = np.random.random((10,3,1))
-imagePoints = np.random.random((10,2,1))
+import cv2, numpy as np, sys
+
+objectPoints = np.array([[18,0,45], [27,0,27], [9,0,63], [54,0,54], [0,0,36]], dtype=np.float32)
+imagePoints = np.array([[527,339], [673,409], [424,285], [708,283], [357,394]], dtype=np.float32)
+imagePoints = 2.6 * imagePoints
 
 #intrinsic matrix
 cameraMatrix = np.array([[1258.513767, 0.000000, 949.143263],
@@ -13,7 +21,7 @@ cameraMatrix = np.array([[1258.513767, 0.000000, 949.143263],
 #distortion coefficients
 distCoeffs = np.array([[-0.350545], [0.098685], [-0.004605], [-0.001945], [0.000000]])
 
-retval, rvec, tvec = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs)
+retval, rvec, tvec = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, flags=cv2.SOLVEPNP_ITERATIVE)
 print "rotation vector:"
 print rvec
 print "translation vector:"
@@ -30,7 +38,7 @@ print r_t_max
 
 A = np.matmul(cameraMatrix, r_t_max)
 print A
-A = np.delete(A,2,1)
+A = np.delete(A,1,1)
 print A
 
 #compute inverse of r_t
