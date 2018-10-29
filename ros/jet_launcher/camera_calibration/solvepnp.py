3f6074c661137d49242b74687d9268d4714da999
John Seng <jseng@calpoly.edu>
2018-10-29 15:48:04 -0700

    update

--- ros/jet_launcher/camera_calibration/solvepnp.py
+++ ros/jet_launcher/camera_calibration/solvepnp.py
@@ -9,9 +9,19 @@
 
 import cv2, numpy as np, sys
 
-objectPoints = np.array([[18,0,45], [27,0,27], [9,0,63], [54,0,54], [0,0,36]], dtype=np.float32)
-imagePoints = np.array([[527,339], [673,409], [424,285], [708,283], [357,394]], dtype=np.float32)
-imagePoints = 2.6 * imagePoints
+objectPoints = np.array([[18,0,45], 
+                        [27,0,27], 
+                        [9,0,63], 
+                        [54,0,54], 
+                        [0,0,36]], dtype=np.float32)
+objectPoints = .0254*objectPoints #convert to meters
+
+imagePoints = np.array([[527,339], 
+                        [673,409], 
+                        [424,285], 
+                        [708,283], 
+                        [357,394]], dtype=np.float32)
+imagePoints = 2.6 * imagePoints #scale to 1920x1080
 
 #intrinsic matrix
 cameraMatrix = np.array([[1258.513767, 0.000000, 949.143263],
@@ -21,8 +31,8 @@ cameraMatrix = np.array([[1258.513767, 0.000000, 949.143263],
 #distortion coefficients
 distCoeffs = np.array([[-0.350545], [0.098685], [-0.004605], [-0.001945], [0.000000]])
 
-#retval, rvec, tvec = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, flags=cv2.SOLVEPNP_ITERATIVE)
-retval, rvec, tvec = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs)
+retval, rvec, tvec = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+#retval, rvec, tvec = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs)
 print "rotation vector:"
 print rvec
 print "translation vector:"
@@ -50,4 +60,7 @@ print r_t_inv
 
 print "---testing---"
 imagepoint = np.array([2.6*527,2.6*339,1],dtype=np.float32)
-print np.matmul(r_t_inv, imagepoint)
+ans = np.matmul(r_t_inv, imagepoint)
+ans = ans / .0254 #convert back to inches
+print ans
+print objectPoints
--- test_vrep/test.py
+++ test_vrep/test.py
@@ -1,18 +1,72 @@
 import vrep
 import sys
 
-vrep.simxFinish(-1) #close all opened connections
-clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
 
-if clientID != -1:
-   print ("Connected to remote API server")
-else:
-   print ("Not connected to remote API server")
-   sys.exit("Could not connect")
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+def initConnection():
+   global clientID
+
+   vrep.simxFinish(-1) #close all opened connections
+   clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+
+   if clientID != -1:
+      print ("Connected to remote API server")
+   else:
+      print ("Not connected to remote API server")
+      sys.exit("Could not connect")
+
+clientID = 0
+initConnection()
 
 err_code,l_motor_handle = vrep.simxGetObjectHandle(clientID,"Pioneer_p3dx_leftMotor", vrep.simx_opmode_blocking)
 err_code,r_motor_handle = vrep.simxGetObjectHandle(clientID,"Pioneer_p3dx_rightMotor", vrep.simx_opmode_blocking)
 
-print err_code
+getch = _Getch()
 
-err_code = vrep.simxSetJointTargetVelocity(clientID,l_motor_handle,1.0,vrep.simx_opmode_blocking)
+while True:
+   a = raw_input('')
+   print a
+   if a == 's':
+      err_code = vrep.simxSetJointTargetVelocity(clientID,l_motor_handle,0,vrep.simx_opmode_blocking)
+   else:
+      err_code = vrep.simxSetJointTargetVelocity(clientID,l_motor_handle,1.0,vrep.simx_opmode_blocking)
+
+
+#key = getch()
+   #print key
+
+vrep.simxFinish(-1) #close all opened connections
