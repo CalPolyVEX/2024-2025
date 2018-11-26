8d03a684881b04b84ce15f6d1a8f461dfc5d8979
John Seng <jseng@calpoly.edu>
2018-11-26 11:17:56 -0800

    update

--- localization/monovo-python/visual_odometry.py
+++ localization/monovo-python/visual_odometry.py
@@ -11,30 +11,31 @@ lk_params = dict(winSize  = (21, 21),
              	criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
 
 def featureTracking(image_ref, image_cur, px_ref):
-	kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, px_ref, None, **lk_params)  #shape: [k,2] [k,1] [k,1]
+   kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, px_ref, None, **lk_params)  #shape: [k,2] [k,1] [k,1]
 
-	st = st.reshape(st.shape[0])
-	kp1 = px_ref[st == 1]
-	kp2 = kp2[st == 1]
+   st = st.reshape(st.shape[0])
+   kp1 = px_ref[st == 1]
+   kp2 = kp2[st == 1]
 
-	return kp1, kp2
+   return kp1, kp2
 
 
 class PinholeCamera:
 	def __init__(self, width, height, fx, fy, cx, cy, 
 				k1=0.0, k2=0.0, p1=0.0, p2=0.0, k3=0.0):
-		self.width = width
-		self.height = height
-		self.fx = fx
-		self.fy = fy
-		self.cx = cx
-		self.cy = cy
-		self.distortion = (abs(k1) > 0.0000001)
-		self.d = [k1, k2, p1, p2, k3]
+            self.width = width
+            self.height = height
+            self.fx = fx
+            self.fy = fy
+            self.cx = cx
+            self.cy = cy
+            self.distortion = (abs(k1) > 0.0000001)
+            self.d = [k1, k2, p1, p2, k3]
 
 
 class VisualOdometry:
-	def __init__(self, cam, annotations):
+	#def __init__(self, cam, annotations):
+	def __init__(self, cam):
 		self.frame_stage = 0
 		self.cam = cam
 		self.new_frame = None
@@ -47,20 +48,24 @@ class VisualOdometry:
 		self.pp = (cam.cx, cam.cy)
 		self.trueX, self.trueY, self.trueZ = 0, 0, 0
 		self.detector = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
-		with open(annotations) as f:
-			self.annotations = f.readlines()
+		#with open(annotations) as f:
+		#	self.annotations = f.readlines()
 
 	def getAbsoluteScale(self, frame_id):  #specialized for KITTI odometry dataset
-		ss = self.annotations[frame_id-1].strip().split()
-		x_prev = float(ss[3])
-		y_prev = float(ss[7])
-		z_prev = float(ss[11])
-		ss = self.annotations[frame_id].strip().split()
-		x = float(ss[3])
-		y = float(ss[7])
-		z = float(ss[11])
-		self.trueX, self.trueY, self.trueZ = x, y, z
-		return np.sqrt((x - x_prev)*(x - x_prev) + (y - y_prev)*(y - y_prev) + (z - z_prev)*(z - z_prev))
+            
+            #JS
+            return .5
+            #end JS
+            ss = self.annotations[frame_id-1].strip().split()
+            x_prev = float(ss[3])
+            y_prev = float(ss[7])
+            z_prev = float(ss[11])
+            ss = self.annotations[frame_id].strip().split()
+            x = float(ss[3])
+            y = float(ss[7])
+            z = float(ss[11])
+            self.trueX, self.trueY, self.trueZ = x, y, z
+            return np.sqrt((x - x_prev)*(x - x_prev) + (y - y_prev)*(y - y_prev) + (z - z_prev)*(z - z_prev))
 
 	def processFirstFrame(self):
 		self.px_ref = self.detector.detect(self.new_frame)
@@ -83,6 +88,7 @@ class VisualOdometry:
 			self.cur_t = self.cur_t + absolute_scale*self.cur_R.dot(t) 
 			self.cur_R = R.dot(self.cur_R)
 		if(self.px_ref.shape[0] < kMinNumFeature):
+                        print "lost"
 			self.px_cur = self.detector.detect(self.new_frame)
 			self.px_cur = np.array([x.pt for x in self.px_cur], dtype=np.float32)
 		self.px_ref = self.px_cur
