b6b4115b785af12d6c91f151151bf7d4dacd85bc
John Seng <jseng@calpoly.edu>
2019-01-15 15:01:00 +0000

    update

--- ros/jet_launcher/launch/record_zed.sh
+++ ros/jet_launcher/launch/record_zed.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-rosbag record --duration=2m /zed/rgb/image_rect_color/compressed /zed/depth/depth_registered/compressedDepth /zed/rgb/camera_info /zed/depth/camera_info /zed/odom /tf /tf_static
+#rosbag record --duration=2m /zed/rgb/image_rect_color/compressed /zed/depth/depth_registered/compressedDepth /zed/rgb/camera_info /zed/depth/camera_info /zed/odom /tf /tf_static
+rosbag record --duration=2m /zed/data_throttled/image_depth/compressedDepth /zed/data_throttled/image/compressed /zed/data_throttled_camera_info /zed/odom /tf /tf_static
