95a9f0cf3d40840aff39c0a703f682424aac8e86
John Seng <jseng@calpoly.edu>
2019-01-28 09:41:44 -0800

    update

--- ros/image_test/nodes/zed_odom_test.py
+++ ros/image_test/nodes/zed_odom_test.py
@@ -12,6 +12,8 @@ from random import randint
 
 from std_msgs.msg import Header
 from nav_msgs.msg import Odometry
+import geometry_msgs.msg
+from tf2_msgs.msg import TFMessage
 
 class range_data_node:
    def __init__(self):
@@ -19,13 +21,35 @@ class range_data_node:
       self.pub = rospy.Publisher("/zed/new_odom", Odometry, queue_size=2)
       self.sub = rospy.Subscriber("/zed/odom", Odometry, self.callback)
 
+      #self.pub = rospy.Publisher("/tf", TFMessage, queue_size=2)
+      #self.sub = rospy.Subscriber("/tf", TFMessage, self.callback_tf)
+
+   def callback_tf(self,data):
+      header = Header()
+      header.stamp = rospy.Time.now()
+      header.frame_id = "odom"
+
+      #fix the Z position
+      data.transforms[0].transform.translation.z = 0  
+
+      #only allow rotations around the Z axis
+      data.transforms[0].transform.rotation.x = 0  
+      data.transforms[0].transform.rotation.y = 0  
+
+      #while not rospy.is_shutdown():
+      #pc2.header.stamp = rospy.Time.now()
+      self.pub.publish(data)
+
+      pass
+
    def callback(self,data):
-      #new_pose = data.pose
+      #fix the Z position
       data.pose.pose.position.z = 0  
+      data.pose.pose.position.y = 0  
+
+      #only allow rotations around the Z axis
       data.pose.pose.orientation.x = 0  
       data.pose.pose.orientation.y = 0  
-      data.pose.pose.orientation.z = 0  
-      data.pose.pose.orientation.w = 1.0000000  
 
       header = Header()
       header.stamp = rospy.Time.now()
