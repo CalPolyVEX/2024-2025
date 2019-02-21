1063f3d19389d03e38fa7c121b374a12fe857e95
John Seng <jseng@calpoly.edu>
2019-02-21 13:46:46 -0800

    update

--- ros/roboclaw_node/nodes/joystick.py
+++ ros/roboclaw_node/nodes/joystick.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 import pygame
 from time import sleep
-import rospy
+import rospy, sys, os
 from geometry_msgs.msg import Quaternion, Twist
 
 class JoystickNode:
    def __init__(self):
-      #self.lock = threading.Lock()
+      #check the number of joysticks
+      pygame.joystick.init()
+      num_joystick = pygame.joystick.get_count()
+      if num_joystick==0:
+         print "No joysticks found."
+         exit(0)
+      
       rospy.init_node("joystick_node",log_level=rospy.DEBUG)
       rospy.on_shutdown(self.shutdown)
       rospy.loginfo("Connecting to joystick")
@@ -18,7 +24,6 @@ class JoystickNode:
       rospy.sleep(1)
 
       pygame.display.init()
-      pygame.joystick.init()
       pygame.joystick.Joystick(0).init()
 
    def run(self):
@@ -36,13 +41,21 @@ class JoystickNode:
       r_time = rospy.Rate(10)
       vel_msg = Twist()
 
+      #do not print SDL messages
+      sys.stdout = os.devnull
+      sys.stderr = os.devnull
+
       while not rospy.is_shutdown():
          pygame.event.pump()
          # Prints the values for axis0
          axis0 = pygame.joystick.Joystick(0).get_axis(0)
          axis1 = pygame.joystick.Joystick(0).get_axis(1)
+         axis2 = pygame.joystick.Joystick(0).get_axis(2)
+         axis3 = pygame.joystick.Joystick(0).get_axis(3)
          rospy.logdebug("axis 0: %f", axis0)
          rospy.logdebug("axis 1: %f", axis1)
+         rospy.logdebug("axis 2: %f", axis2)
+         rospy.logdebug("axis 3: %f", axis3)
          rospy.logdebug("button 0: %d", pygame.joystick.Joystick(0).get_button(0))
 
          vel_msg.linear.x = -1.0 * axis1
@@ -51,6 +64,7 @@ class JoystickNode:
          self.motor_command_pub.publish(vel_msg)
          
          r_time.sleep()
+
    def shutdown(self):
       rospy.loginfo("Shutting down")
 
