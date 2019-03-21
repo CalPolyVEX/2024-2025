9ef39325fa152c8d156196f768d5d6a05d9fabd6
John Seng <jsseng@gmail.com>
2019-03-21 14:40:25 -0700

    update

--- ros/install/installROS.sh
+++ ros/install/installROS.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-# Install Robot Operating System (ROS) on NVIDIA Jetson TX1
-# Maintainer of ARM builds for ROS is http://answers.ros.org/users/1034/ahendrix/
-# Information from:
-# http://wiki.ros.org/kinetic/Installation/UbuntuARM
 
 # Setup Locale
 # sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
 # Setup sources.lst
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 # Installation
 sudo apt-get update
-sudo apt-get install ros-kinetic-ros-base -y
+sudo apt-get install ros-melodic-ros-base -y
 # Add Individual Packages here
 # You can install a specific ROS package (replace underscores with dashes of the package name):
 # sudo apt-get install ros-kinetic-PACKAGE
@@ -25,13 +21,28 @@ sudo apt-get install ros-kinetic-ros-base -y
 # Initialize rosdep
 sudo apt-get install python-rosdep -y
 # ssl certificates can get messed up on TX1 for some reason
-sudo c_rehash /etc/ssl/certs
+#sudo c_rehash /etc/ssl/certs
 # Initialize rosdep
 sudo rosdep init
 # To find available packages, use:
 rosdep update
 # Environment Setup
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 # Install rosinstall
 sudo apt-get install python-rosinstall -y
+
+#setup workspace
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+git init
+git config core.sparseCheckout true
+echo 'ros/' > .git/info/sparse-checkout
+git remote add -f origin https://github.com/jsseng/ue4.git
+git pull origin master
+
+#install dependencies
+apt install python-pygame
+apt install ros-kinetic-rosserial-arduino
+apt install ros-kinetic-tf2-sensor-msgs
+
