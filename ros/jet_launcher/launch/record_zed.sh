69984e1446bab13682cc5332fe69e3eb8f6e8ca7
John Seng <jseng@calpoly.edu>
2019-05-16 14:57:39 -0700

    update

--- ros/jet_launcher/launch/record_zed.sh
+++ ros/jet_launcher/launch/record_zed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
-   rosbag record --duration=2m /zed/data_throttled_image_depth /zed/data_throttled_image /zed/data_throttled_camera_info /zed/depth/camera_info /zed/odom /tf /tf_static 
+   rosbag record --duration=2m /zed/data_throttled_image_depth /zed/data_throttled_image /zed/data_throttled_camera_info /zed/depth/camera_info /camera/odom/sample /tf /tf_static 
    #rosbag record --duration=2m /zed/data_throttled_image_depth /zed/data_throttled_image /zed/data_throttled_camera_info /zed/depth/camera_info /zed/odom /tf /tf_static /zed/throttled_point_cloud
 else
    echo "one argument"
-   rosbag record --duration=2m /zed/data_throttled_image_depth/compressedDepth /zed/data_throttled_image/compressed /zed/data_throttled_camera_info /zed/depth/camera_info /zed/odom /tf /tf_static
+   rosbag record --duration=2m /zed/data_throttled_image_depth/compressedDepth /zed/data_throttled_image/compressed /zed/data_throttled_camera_info /zed/depth/camera_info /camera/odom/sample /tf /tf_static
 fi
