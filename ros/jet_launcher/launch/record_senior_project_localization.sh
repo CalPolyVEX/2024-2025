38a73437ec295a0eb7a73ff9a48ec6b7178fcab0
John Seng <jseng@calpoly.edu>
2019-05-18 16:00:37 -0700

    update

--- ros/jet_launcher/launch/record_senior_project_localization.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 1 ]; then
-   rosbag record --duration=2m /tf /tf_static /see3cam_cu20/image_raw/compressed
-else
-   echo "one argument"
-   rosbag record --duration=2m /zed/data_throttled_image_depth/compressedDepth /zed/data_throttled_image/compressed /zed/data_throttled_camera_info /zed/depth/camera_info /zed/odom /tf /tf_static
-fi
