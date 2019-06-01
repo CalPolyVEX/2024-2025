12b821cf2c5d81c787258fa5026e758a90dee8e8
John Seng <jseng@calpoly.edu>
2019-06-01 14:08:58 -0700

    update

--- ros/transform_point_cloud/src/transform_gyro_msgs.cpp
+++ ros/transform_point_cloud/src/transform_gyro_msgs.cpp
@@ -14,48 +14,97 @@ class GyroPublisher
   ros::NodeHandle nh_;
   ros::Subscriber sub_;
   ros::Publisher pub_;
+  ros::Subscriber sub_a;
+  ros::Publisher pub_a;
 
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
-  sensor_msgs::Imu imu;
+  sensor_msgs::Imu imu_g;
+  sensor_msgs::Imu imu_a;
+  int gyro_counter = 0;
 
   void gyroCallBack(const sensor_msgs::Imu::ConstPtr& msg) {
       /* ros::Time current_time = ros::Time::now(); */
       int i;
 
+      //publish a new message every other incoming gyro message
+      gyro_counter++;
+      if ((gyro_counter & 1) == 0) {
+          return;
+      }
+
       //Imu message
-      imu.header.seq = msg->header.seq;
-      imu.header.stamp = msg->header.stamp;
-      imu.header.frame_id = "t265_link";
+      imu_g.header.seq = msg->header.seq;
+      imu_g.header.stamp = msg->header.stamp;
+      imu_g.header.frame_id = "t265_link";
 
       //set the orientation
-      imu.orientation.x = msg->orientation.x;
-      imu.orientation.y = msg->orientation.y;
-      imu.orientation.z = msg->orientation.z;
-      imu.orientation.w = msg->orientation.w;
+      imu_g.orientation.x = msg->orientation.x;
+      imu_g.orientation.y = msg->orientation.y;
+      imu_g.orientation.z = msg->orientation.z;
+      imu_g.orientation.w = msg->orientation.w;
       for (i=0;i<9;i++) {
-          imu.orientation_covariance[i] = msg->orientation_covariance[i];
+          imu_g.orientation_covariance[i] = msg->orientation_covariance[i];
       }
       
-      //set the angular velocity
-      imu.angular_velocity.x = msg->angular_velocity.x;
-      imu.angular_velocity.y = msg->angular_velocity.y;
-      imu.angular_velocity.z = msg->angular_velocity.z;
+      //set the angular velocity - rotate the axes to be correct for the
+      //robot_localization package
+      imu_g.angular_velocity.x = msg->angular_velocity.z;
+      imu_g.angular_velocity.y = msg->angular_velocity.x;
+      imu_g.angular_velocity.z = msg->angular_velocity.y;
       for (i=0;i<9;i++) {
-          imu.angular_velocity_covariance[i] = msg->angular_velocity_covariance[i];
+          imu_g.angular_velocity_covariance[i] = msg->angular_velocity_covariance[i];
       }
       
       //set the linear acceleration
-      imu.linear_acceleration.x = msg->linear_acceleration.x;
-      imu.linear_acceleration.y = msg->linear_acceleration.y;
-      imu.linear_acceleration.z = msg->linear_acceleration.z;
+      imu_g.linear_acceleration.x = msg->linear_acceleration.x;
+      imu_g.linear_acceleration.y = msg->linear_acceleration.y;
+      imu_g.linear_acceleration.z = msg->linear_acceleration.z;
       for (i=0;i<9;i++) {
-          imu.linear_acceleration_covariance[i] = msg->linear_acceleration_covariance[i];
+          imu_g.linear_acceleration_covariance[i] = msg->linear_acceleration_covariance[i];
       }
 
       //publish the message
+      pub_.publish(imu_g);
+  }
 
-      pub_.publish(imu);
+  void accelCallBack(const sensor_msgs::Imu::ConstPtr& msg) {
+      /* ros::Time current_time = ros::Time::now(); */
+      int i;
+
+      //Imu message
+      imu_a.header.seq = msg->header.seq;
+      imu_a.header.stamp = msg->header.stamp;
+      imu_a.header.frame_id = "t265_link";
+
+      //set the orientation
+      imu_a.orientation.x = 0;
+      imu_a.orientation.y = 0;
+      imu_a.orientation.z = 0;
+      imu_a.orientation.w = 0;
+      for (i=0;i<9;i++) {
+          imu_a.orientation_covariance[i] = msg->orientation_covariance[i];
+      }
+      
+      //set the angular velocity
+      imu_a.angular_velocity.x = 0;
+      imu_a.angular_velocity.y = 0;
+      imu_a.angular_velocity.z = 0;
+      for (i=0;i<9;i++) {
+          imu_a.angular_velocity_covariance[i] = msg->angular_velocity_covariance[i];
+      }
+      
+      //set the linear acceleration - rotate the axes to be correct for the
+      //robot_localization package
+      imu_a.linear_acceleration.x = msg->linear_acceleration.z;
+      imu_a.linear_acceleration.y = msg->linear_acceleration.x;
+      imu_a.linear_acceleration.z = msg->linear_acceleration.y;
+      for (i=0;i<9;i++) {
+          imu_a.linear_acceleration_covariance[i] = msg->linear_acceleration_covariance[i];
+      }
+
+      //publish the message
+      pub_a.publish(imu_a);
   }
 
     public:
@@ -63,8 +112,10 @@ class GyroPublisher
   GyroPublisher() :
       tf_listener_(tf_buffer_)
     {
-        pub_ = nh_.advertise<sensor_msgs::Imu>("/new_gyro_data", 3);
+        pub_ = nh_.advertise<sensor_msgs::Imu>("/t265_gyro_data", 3);
         sub_ = nh_.subscribe("/camera/gyro/sample", 1, &GyroPublisher::gyroCallBack, this);
+        pub_a = nh_.advertise<sensor_msgs::Imu>("/t265_accel_data", 3);
+        sub_a = nh_.subscribe("/camera/accel/sample", 1, &GyroPublisher::accelCallBack, this);
     }
 };
 
