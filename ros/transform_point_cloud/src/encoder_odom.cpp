08b3a2b67d6794a2671b70ede219bd91272b7242
John Seng <jsseng@gmail.com>
2019-06-15 07:47:35 -0700

    update

--- ros/transform_point_cloud/src/encoder_odom.cpp
+++ ros/transform_point_cloud/src/encoder_odom.cpp
@@ -1,7 +1,3 @@
-
-//6/1/19 This is a currently unused file to publish odometry when 
-//each encoder message arrives
-//
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
@@ -12,122 +8,224 @@
 #include <ros/console.h>
 #include <serial/serial.h>
 #include <iostream>
+#include "encoder_odom.h"
 using namespace std;
 
 extern ros::NodeHandle nh;
 extern ros::Subscriber sub_;
 extern ros::Publisher pub_, odom_req;
 
-class EncoderOdom {
-  int _PreviousLeftEncoderCounts, _PreviousRightEncoderCounts;
-  ros::Time last_time;
-  double x,y,th;
-  double MAX_ABS_LINEAR_SPEED, MAX_ABS_ANGULAR_SPEED, TICKS_PER_METER, BASE_WIDTH, ACC_LIM;
+void OdometryPublisher::publish_odometry_message(void) {
+  int tick_x = 0;
+  int tick_y = 0;
 
-  //left_integral = [x for x in range(5)]
-  //right_integral = [x for x in range(5)]
-  int left_counter, right_counter, left_pwm, right_pwm;
-  ros::Time last_set_speed_time;
-  int last_left_error, last_right_error;
-  int vl, vr;
+  ros::Time current_time = ros::Time::now();
+  double DistancePerCount = (3.14159265 * 0.13) / 2626; 
+  double lengthBetweenTwoWheels = 0.25;
 
-  tf2_ros::Buffer tf_buffer_;
-  tf2_ros::TransformListener tf_listener_;
+  //extract the wheel velocities from the tick signals count
+  int deltaLeft = tick_x - _PreviousLeftEncoderCounts;
+  int deltaRight = tick_y - _PreviousRightEncoderCounts;
 
-  void odometryCallBack(const std_msgs::Int32MultiArray::ConstPtr& msg) {
-    int tick_x = 0;
-    int tick_y = 0;
+  double omega_left = (deltaLeft * DistancePerCount) / (current_time - last_time).toSec();
+  double omega_right = (deltaRight * DistancePerCount) / (current_time - last_time).toSec();
 
-    ros::Time current_time = ros::Time::now();
-    double DistancePerCount = (3.14159265 * 0.13) / 2626; 
-    double lengthBetweenTwoWheels = 0.25;
+  double v_left = omega_left * 0.065; //radius
+  double v_right = omega_right * 0.065;
 
-    //extract the wheel velocities from the tick signals count
-    int deltaLeft = tick_x - _PreviousLeftEncoderCounts;
-    int deltaRight = tick_y - _PreviousRightEncoderCounts;
+  double vx = ((v_right + v_left) / 2)*10;
+  double vy = 0;
+  double vth = ((v_right - v_left)/lengthBetweenTwoWheels)*10;
 
-    double omega_left = (deltaLeft * DistancePerCount) / (current_time - last_time).toSec();
-    double omega_right = (deltaRight * DistancePerCount) / (current_time - last_time).toSec();
+  double dt = (current_time - last_time).toSec();
+  double delta_x = (vx * cos(th)) * dt;
+  double delta_y = (vx * sin(th)) * dt;
+  double delta_th = vth * dt;
 
-    double v_left = omega_left * 0.065; //radius
-    double v_right = omega_right * 0.065;
+  x += delta_x;
+  y += delta_y;
+  th += delta_th;
 
-    double vx = ((v_right + v_left) / 2)*10;
-    double vy = 0;
-    double vth = ((v_right - v_left)/lengthBetweenTwoWheels)*10;
+  tf2::Quaternion odom_quat;
+  odom_quat.setRPY(0,0,0);
 
-    double dt = (current_time - last_time).toSec();
-    double delta_x = (vx * cos(th)) * dt;
-    double delta_y = (vx * sin(th)) * dt;
-    double delta_th = vth * dt;
+  geometry_msgs::TransformStamped odom_trans;
+  odom_trans.header.stamp = current_time;
+  odom_trans.header.frame_id = "robot_odom";
+  odom_trans.child_frame_id = "base_link";
 
-    x += delta_x;
-    y += delta_y;
-    th += delta_th;
+  odom_trans.transform.translation.x = x;
+  odom_trans.transform.translation.y = y;
+  odom_trans.transform.translation.z = 0.0;
+  //odom_trans.transform.rotation = odom_quat;
 
-    tf2::Quaternion odom_quat;
-    odom_quat.setRPY(0,0,0);
+  //send the transform
+  //odom_broadcaster.sendTransform(odom_trans);
 
-    geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "robot_odom";
-    odom_trans.child_frame_id = "base_link";
+  //Odometry message
+  nav_msgs::Odometry odom;
+  odom.header.stamp = current_time;
+  odom.header.frame_id = "odom";
 
-    odom_trans.transform.translation.x = x;
-    odom_trans.transform.translation.y = y;
-    odom_trans.transform.translation.z = 0.0;
-    //odom_trans.transform.rotation = odom_quat;
+  //set the position
+  odom.pose.pose.position.x = x;
+  odom.pose.pose.position.y = y;
+  odom.pose.pose.position.z = 0.0;
+  //odom.pose.pose.orientation = odom_quat;
 
-    //send the transform
-    //odom_broadcaster.sendTransform(odom_trans);
+  //set the velocity
+  odom.child_frame_id = "base_link";
+  odom.twist.twist.linear.x = vx;
+  odom.twist.twist.linear.y = vy;
+  odom.twist.twist.angular.z = vth;
 
-    //Odometry message
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = "odom";
+  //publish the message
+  pub_.publish(odom);
+  _PreviousLeftEncoderCounts = tick_x;
+  _PreviousRightEncoderCounts = tick_y;
 
-    //set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
-    odom.pose.pose.position.z = 0.0;
-    //odom.pose.pose.orientation = odom_quat;
+  last_time = current_time;
+}
 
-    //set the velocity
-    odom.child_frame_id = "base_link";
-    odom.twist.twist.linear.x = vx;
-    odom.twist.twist.linear.y = vy;
-    odom.twist.twist.angular.z = vth;
-
-    //publish the message
-    pub_.publish(odom);
-    _PreviousLeftEncoderCounts = tick_x;
-    _PreviousRightEncoderCounts = tick_y;
-
-    last_time = current_time;
+double OdometryPublisher::normalize_angle(double angle) {
+  double pi = 3.14159;
+  while (angle > pi) {
+    angle -= 2.0 * pi;
+  }
+  while (angle < -pi) {
+    angle += 2.0 * pi;
   }
 
-    public:
+  return angle;
+}
 
-  EncoderOdom() : tf_listener_(tf_buffer_) {
-    nh.param<double>("max_abs_linear_speed", MAX_ABS_LINEAR_SPEED, 1.0);
-    nh.param<double>("max_abs_angular_speed", MAX_ABS_ANGULAR_SPEED, 1.0);
-    nh.param<double>("ticks_per_meter", TICKS_PER_METER, 6683);
-    nh.param<double>("base_width", BASE_WIDTH, 0.315);
-    nh.param<double>("acc_lim", ACC_LIM, 0.1);
+void OdometryPublisher::update(int enc_left, int enc_right, double* vel_x, double* vel_theta) {
+  int left_ticks, right_ticks;
+  double dist_left, dist_right, dist;
+  double d_theta, r;
+  ros::Time current_time;
 
-    //left_integral = [x for x in range(5)]
-    //right_integral = [x for x in range(5)]
-    left_counter = 0;
-    right_counter = 0;
-    left_pwm = 0; //current PWM values sent to Roboclaw
-    right_pwm = 0;
-    last_set_speed_time = ros::Time::now();
-    last_left_error = 0;
-    last_right_error = 0;
-    vl = 0;
-    vr = 0;
+  //take the encoder counts and update the number of ticks traveled
+  left_ticks = enc_left - last_enc_left;
+  right_ticks = enc_right - last_enc_right;
+  last_enc_left = enc_left;
+  last_enc_right = enc_right;
 
-    /*         rospy.sleep(1) */
+  //compute the distance traveled by each wheel
+  dist_left = left_ticks / TICKS_PER_METER;
+  dist_right = right_ticks / TICKS_PER_METER;
+  dist = (dist_right + dist_left) / 2.0;
+
+  //compute elapsed time
+  current_time = ros::Time::now();
+  double d_time = (current_time - last_enc_time).toSec();
+  last_enc_time = current_time;
+
+  double current_left_vel = left_ticks / d_time; //ticks/second
+  double current_right_vel = right_ticks / d_time;
+
+  //if last_left_vel != 0 and last_right_vel != 0:
+
+  //when computing the current tick velocity, filter the readings
+  //from the encoders as the readings are noisy at slow speeds
+  left_tick_vel =  .7*left_tick_vel + .3*(left_ticks / d_time); //should be in ticks/second
+  right_tick_vel = .7*right_tick_vel + .3*(right_ticks / d_time);
+
+  // TODO find better what to determine going straight,
+  // this means slight deviation is accounted
+  if (left_ticks == right_ticks) {
+    d_theta = 0.0;
+    cur_x += dist * cos(cur_theta);
+    cur_y += dist * sin(cur_theta);
+  } else {
+    d_theta = (dist_right - dist_left) / BASE_WIDTH;
+    r = dist / d_theta;
+    cur_x += r * (sin(d_theta + cur_theta) -
+        sin(cur_theta));
+    cur_y -= r * (cos(d_theta + cur_theta) -
+        cos(cur_theta));
+    cur_theta = normalize_angle(cur_theta - d_theta);
   }
-};
 
+  if (abs(d_time) < 0.000001) {
+    *vel_x = 0.0;
+    *vel_theta = 0.0;
+  } else {
+    *vel_x = dist / d_time;
+    *vel_theta = d_theta / d_time;
+  }
+
+  return;
+}
+
+void OdometryPublisher::update_publish_cb(const std_msgs::Int32MultiArray::ConstPtr& enc_msg) {
+  //this callback is called when a new encoder message is
+  //received from the Arduino
+  int enc_left, enc_right;
+
+  enc_left = enc_msg->data[0];
+  enc_right = enc_msg->data[1];
+
+  //2106 per 0.1 seconds is max speed, error in the 16th bit is 32768
+  //todo lets find a better way to deal with this error
+  if (abs(enc_left - last_enc_left) > 20000) {
+    ROS_INFO("Ignoring left encoder jump: cur %d, last %d",  enc_left, last_enc_left);
+  } else if (abs(enc_right - last_enc_right) > 20000) {
+    ROS_INFO("Ignoring right encoder jump: cur %d, last %d", enc_right, last_enc_right);
+  } else {
+    //call the update function
+    //vel_x, vel_theta = update(enc_left, enc_right);
+    //publish_odometry_message(cur_x, cur_y, cur_theta, vel_x, vel_theta)
+  }
+
+
+            /*
+    def update( enc_left, enc_right):
+        #take the encoder counts and update the number of ticks traveled
+        left_ticks = enc_left - last_enc_left
+        right_ticks = enc_right - last_enc_right
+        last_enc_left = enc_left
+        last_enc_right = enc_right
+
+        dist_left = left_ticks / TICKS_PER_METER
+        dist_right = right_ticks / TICKS_PER_METER
+        dist = (dist_right + dist_left) / 2.0
+
+        current_time = rospy.Time.now()
+        d_time = (current_time - last_enc_time).to_sec()
+        last_enc_time = current_time
+
+        last_left_vel = left_ticks / d_time #ticks/second
+        last_right_vel = right_ticks / d_time
+        #if last_left_vel != 0 and last_right_vel != 0:
+
+        #when computing the current tick velocity, filter the readings
+        #from the encoders as the readings are noisy at slow speeds
+        left_tick_vel =  .7*left_tick_vel + .3*(left_ticks / d_time) #should be in ticks/second
+        right_tick_vel = .7*right_tick_vel + .3*(right_ticks / d_time)
+
+        # TODO find better what to determine going straight,
+        # this means slight deviation is accounted
+        if left_ticks == right_ticks:
+            d_theta = 0.0
+            cur_x += dist * cos(cur_theta)
+            cur_y += dist * sin(cur_theta)
+        else:
+            d_theta = (dist_right - dist_left) / BASE_WIDTH
+            r = dist / d_theta
+            cur_x += r * (sin(d_theta + cur_theta) -
+                               sin(cur_theta))
+            cur_y -= r * (cos(d_theta + cur_theta) -
+                               cos(cur_theta))
+            cur_theta = normalize_angle(cur_theta - d_theta)
+
+        if abs(d_time) < 0.000001:
+            vel_x = 0.0
+            vel_theta = 0.0
+        else:
+            vel_x = dist / d_time
+            vel_theta = d_theta / d_time
+
+        return vel_x, vel_theta
+*/
+}
--- ros/transform_point_cloud/src/encoder_odom.h
+++ ros/transform_point_cloud/src/encoder_odom.h
@@ -1,3 +1,6 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
@@ -8,19 +11,34 @@
 #include <ros/console.h>
 #include <serial/serial.h>
 #include <iostream>
-using namespace std;
 
-class EncoderOdom {
+class OdometryPublisher {
   int _PreviousLeftEncoderCounts, _PreviousRightEncoderCounts;
-  ros::Time last_time;
+  ros::Time last_time, last_enc_time;
   double x,y,th;
   double MAX_ABS_LINEAR_SPEED, MAX_ABS_ANGULAR_SPEED, TICKS_PER_METER, BASE_WIDTH, ACC_LIM;
 
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
 
-public:
-  EncoderOdom();
-  void odometryCallBack(const std_msgs::Int32MultiArray::ConstPtr& msg);
+  //left_integral = [x for x in range(5)]
+  //right_integral = [x for x in range(5)]
+  int left_counter, right_counter, left_pwm, right_pwm;
+  ros::Time last_set_speed_time;
+  int last_left_error, last_right_error;
+  int vl, vr;
+  int last_enc_left, last_enc_right;
+  double last_left_vel, last_right_vel;
+  double left_tick_vel, right_tick_vel;
+  double cur_x=0, cur_y=0, cur_theta=0;
+
+  public:
+    OdometryPublisher(); 
+    void odometryCallBack(const std_msgs::Int32MultiArray::ConstPtr& msg);
+    double normalize_angle(double angle);
+    void publish_odometry_message(void); //publish a new Odometry message
+    void update(int enc_left, int enc_right, double* vel_x, double* vel_theta);
+    void update_publish_cb(const std_msgs::Int32MultiArray::ConstPtr& enc_msg); //called when a new encoder messaged received from the Arduino
 };
 
+#endif
