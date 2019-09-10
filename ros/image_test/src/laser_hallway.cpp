42008ee537412981ddb1de98fbcd59eb00d134e0
John Seng <jseng@calpoly.edu>
2019-09-09 18:45:21 -0700

    test laser_hallway node

--- ros/image_test/src/laser_hallway.cpp
+++ ros/image_test/src/laser_hallway.cpp
@@ -8,12 +8,19 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/video/tracking.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/types.hpp>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointField.h>
 #include <sensor_msgs/LaserScan.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <laser_geometry/laser_geometry.h>
+
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_line.h>
 
 #define _USE_MATH_DEFINES
 #include <cmath>
@@ -28,84 +35,53 @@ class ScanProcessor {
   ros::Publisher point_pub;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
+  laser_geometry::LaserProjection projector_;
  
   public:
 
-  void convert_scan(const sensor_msgs::LaserScan& msg) {
-  }
+  void convert_scan(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    sensor_msgs::PointCloud cloud;
+    projector_.projectLaser(*msg, cloud);
 
-  void publish_point_cloud() {
-    // Fill in new PointCloud2 message (2D image-like layout)
-    sensor_msgs::PointCloud2 points_msg = sensor_msgs::PointCloud2();
+    // // initialize PointClouds
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1 (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr final1 (new pcl::PointCloud<pcl::PointXYZ>);
 
-    /* points_msg->header = disp_msg->header; */
-    points_msg.header.stamp = ros::Time::now();
-    points_msg.header.seq = 0;
-    points_msg.header.frame_id = "zed_camera_center";
+    // populate our PointCloud with points
+    cloud1->width    = 100;
+    cloud1->height   = 1;
+    cloud1->is_dense = false;
+    cloud1->points.resize (cloud1->width * cloud1->height);
 
-    points_msg.height = 1;
-    points_msg.width  = 48;
-    points_msg.fields.resize (4);
-    points_msg.fields[0].name = "x";
-    points_msg.fields[0].offset = 0;
-    points_msg.fields[0].count = 1;
-    points_msg.fields[0].datatype = sensor_msgs::PointField::FLOAT32;
-    points_msg.fields[1].name = "y";
-    points_msg.fields[1].offset = 4;
-    points_msg.fields[1].count = 1;
-    points_msg.fields[1].datatype = sensor_msgs::PointField::FLOAT32;
-    points_msg.fields[2].name = "z";
-    points_msg.fields[2].offset = 8;
-    points_msg.fields[2].count = 1;
-    points_msg.fields[2].datatype = sensor_msgs::PointField::FLOAT32;
-    points_msg.fields[3].name = "rgb";
-    points_msg.fields[3].offset = 12;
-    points_msg.fields[3].count = 1;
-    points_msg.fields[3].datatype = sensor_msgs::PointField::UINT32;
-    //points_msg->is_bigendian = false; ???
-    static const int STEP = 16;
-    points_msg.point_step = STEP;
-    points_msg.row_step = points_msg.point_step * points_msg.width;
-    points_msg.data.resize (points_msg.row_step * points_msg.height);
-    points_msg.is_dense = true; // there are no invalid points
+    int i=0;
+    for (auto it = begin (cloud.points); it != end (cloud.points); ++it) {
+      float x = it->x;
+      float y = it->y;
 
-    int offset = 0;
-    float cur_x = 5; //the starting x coordinate
-    float z = .02; //the ground is a plane so the Z-coordinate is 0
-    for (int u = 0; u < 48 ; u++) { //go through the 48 points
-      float img_x = cur_x;
-      float img_y = 0;
-
-      double c_x,c_y;
-      transform_point(img_x, img_y, &c_x, &c_y);
-      float temp_x, temp_y;
-      temp_x = -c_x;
-      temp_y = c_y;
-
-      // x,y,z
-      memcpy (&points_msg.data[offset + 0], &temp_y, sizeof (float));
-      memcpy (&points_msg.data[offset + 4], &temp_x, sizeof (float));
-      memcpy (&points_msg.data[offset + 8], &z, sizeof (float));
-
-      //set the color
-      unsigned int color = 0xff0000ff;
-      memcpy (&points_msg.data[offset + 12], &color, sizeof (int));
-
-      cur_x += 10;
-      offset += 16;
+      float dist = sqrt(x*x + y*y); 
+      if (dist < 3.0) { //distance is less than 3.0m
+          cloud1->points[i].x = x;
+          cloud1->points[i].y = y;
+          cloud1->points[i].z = 0;
+          i++;
+      }
     }
 
-    point_pub.publish(points_msg);
-  }
+    //run RANSAC
+    std::vector<int> inliers;
+    pcl::SampleConsensusModelLine<pcl::PointXYZ>::Ptr model_l(new pcl::SampleConsensusModelLine<pcl::PointXYZ> (cloud1));
 
-  void transform_point(double x, double y, double* x_object, double* y_object) {
+    pcl::RandomSampleConsensus<pcl::PointXYZ> ransac (model_l);
+    ransac.setDistanceThreshold (.01);
+    ransac.computeModel();
+    ransac.getInliers(inliers);
+    ROS_INFO("original size: %d", i);
+    ROS_INFO("inliers size: %d", (int)inliers.size());
   }
 
   ScanProcessor() : tf_listener_(tf_buffer_) {
-
-    scan_sub = nh->subscribe("/see3cam_cu20/image_raw", 1, &ScanProcessor::convert_scan, this);
+    scan_sub = nh->subscribe("/laser_scan_filtered", 1, &ScanProcessor::convert_scan, this);
     point_pub = nh->advertise<sensor_msgs::PointCloud2>("/test_point_cloud", 1);
-
   }
 
 };
