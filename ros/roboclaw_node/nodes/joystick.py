68826f7824c8142887c3a79b05630d3efb679fc8
John Seng <jseng@calpoly.edu>
2019-09-11 09:30:10 -0700

    added better recording for debugging

--- ros/roboclaw_node/nodes/joystick.py
+++ ros/roboclaw_node/nodes/joystick.py
@@ -81,7 +81,8 @@ class JoystickNode:
       l.close()
       rec_topics = "rosbag record /zed/data_throttled_image_depth \
          /zed/data_throttled_image /zed/data_throttled_camera_info /laser_scan_filtered \
-         /tf /tf_static /ekf_node/odom /voxel_grid/output /obstacles_cloud \
+         /tf /tf_static /ekf_node/odom /obstacles_cloud \
+         /autonomous /map \
          __name:=my_bag_recorder"
       proc1 = subprocess.Popen('cd /mnt/temp;' + rec_topics, shell=True)
       #rosbag record -o /file/name /topic __name:=my_bag
