a6471375d0c54e6a0c363568ea4cbd3120fe0e39
jsseng <jseng@calpoly.edu>
2019-12-22 06:55:13 -0800

    Update installROS.sh

--- ros/install/installROS.sh
+++ ros/install/installROS.sh
@@ -67,4 +67,9 @@ git config --global user.name "John Seng"
 #sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 #sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 
+#disable graphical boot
+systemctl set-default multi-user.target
+
+#remove rtabmap
+
 
