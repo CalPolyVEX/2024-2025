687bd0a73d62fc4b0dfcef7e45fb9e5ccae06481
John Seng <jseng@calpoly.edu>
2020-01-15 20:03:57 -0800

    updated image save script

--- ros/image_test/nodes/old/save_images.py
+++ ros/image_test/nodes/old/save_images.py
@@ -2,6 +2,9 @@
 #This script saves .jpg images from the /image_raw_throttle topic
 #That topic typically publishes at 1Hz
 
+#playback the .bag file
+#rosrun image_transport republish compressed in:=/see3cam_cu20/image_raw raw out:=/see3cam_cu20/raw
+
 from __future__ import print_function
 
 import roslib
@@ -9,7 +12,7 @@ roslib.load_manifest('image_test')
 import sys
 import rospy
 import cv2
-import time
+import time, datetime
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -21,7 +24,8 @@ class image_converter:
       self.image_pub = rospy.Publisher("/see3cam_cu20/test_image_topic_2",Image,queue_size=1)
 
       self.bridge = CvBridge()
-      self.image_sub = rospy.Subscriber("/see3cam_cu20/image_raw_throttle",Image,self.callback)
+      self.image_sub = rospy.Subscriber("/see3cam_cu20/raw",Image,self.callback)
+      self.last_time = time.time()
 
    def callback(self,data):
       try:
@@ -29,29 +33,36 @@ class image_converter:
       except CvBridgeError as e:
          print(e)
 
-
       #save the file
       now = time.time()
-      localtime = time.localtime(now)
-      milliseconds = '%03d' % int((now - int(now)) * 1000)
-      timestamp = time.strftime('%H-%M-%S-%m-%d-%Y-', localtime) + milliseconds
-      filename = str(timestamp)+'.jpg'
+      print(now)
+      if (now - self.last_time) > 1.0:
+         milliseconds = '%03d' % int((now - int(now)) * 1000)
+         #timestamp = time.strftime('%H-%M-%S-%m-%d-%Y-', now) + milliseconds
+         filename = str(datetime.datetime.now().replace(microsecond=0).isoformat())+'.jpg'
 
-      print ('writing file ' + str(self.counter) + ': ' + filename)
-      self.counter += 1
-      cv2.imwrite(filename, cv_image)
+         print ('writing file ' + str(self.counter) + ': ' + filename)
+         self.counter += 1
+         cv2.imwrite(filename, cv_image)
 
-      (rows,cols,channels) = cv_image.shape
-      if cols > 60 and rows > 60 :
-         cv2.circle(cv_image, (50,50), 20, 255)
+         (rows,cols,channels) = cv_image.shape
+         if cols > 60 and rows > 60 :
+            cv2.circle(cv_image, (50,50), 20, 255)
 
-      #cv2.imshow("Image window", cv_image)
-      #cv2.waitKey(3)
+         #cv2.imshow("Image window", cv_image)
+         #cv2.waitKey(3)
 
-      try:
-         self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-      except CvBridgeError as e:
-         print(e)
+         try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+         except CvBridgeError as e:
+            print(e)
+
+         self.last_time = now
+         if self.counter == 10:
+            self.image_sub.unregister()
+            print ("Exiting...")
+            sys.exit(0)
+            rospy.signal_shutdown()
 
 def main(args):
    ic = image_converter()
