9f9e65a538d0822be7caec1beaa9c7036c4958ff
John Seng <jseng@calpoly.edu>
2021-03-31 14:06:00 -0700

    add test loop

--- herbie_nn/ground_detection/c_test/test.cpp
+++ herbie_nn/ground_detection/c_test/test.cpp
@@ -1,7 +1,12 @@
 #include <torch/script.h> // One-stop header.
+#include <torch/torch.h>
+
+//#include "ros/ros.h"
 
 #include <iostream>
 #include <memory>
+#include <chrono>
+using namespace std::chrono;
 
 int main(int argc, const char* argv[]) {
   if (argc != 2) {
@@ -9,16 +14,34 @@ int main(int argc, const char* argv[]) {
     return -1;
   }
 
-
   torch::jit::script::Module module;
   try {
     // Deserialize the ScriptModule from a file using torch::jit::load().
-    module = torch::jit::load(argv[1]);
+    module = torch::jit::load(argv[1], torch::kCUDA);
   }
   catch (const c10::Error& e) {
     std::cerr << "error loading the model\n";
     return -1;
   }
 
-  std::cout << "ok\n";
+  std::cout << "model loaded successfully.\n";
+
+  for(int i=0; i<1000000; i++) {
+    auto input = torch::randn({3, 360, 640});
+    input = input.unsqueeze(0);
+    auto start = high_resolution_clock::now();
+
+    // Create a vector of inputs.
+    torch::Tensor gpu_tensor = input.to(torch::kCUDA);
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(gpu_tensor);
+    
+    // Execute the model and turn its output into a tensor.
+    at::Tensor output = module.forward(inputs).toTensor();
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end - start) / 1000.0;
+    std::cout << output.slice(1,0,8) << std::endl;
+    std::cout << duration.count() << std::endl;
+  }
 }
