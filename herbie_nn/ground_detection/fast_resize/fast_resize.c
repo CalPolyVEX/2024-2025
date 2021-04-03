2975d3008cdd809b78577a2bb40864fbbb6ec400
John Seng <jseng@calpoly.edu>
2021-04-02 21:54:25 -0700

    converted average to shifts

--- herbie_nn/ground_detection/fast_resize/fast_resize.c
+++ herbie_nn/ground_detection/fast_resize/fast_resize.c
@@ -10,84 +10,106 @@ void process_uyvy(unsigned char* buffer) {
     unsigned char ba_new[360*640*3];
     int row_size = 3840; //number of bytes per row
     int index=0;
-    float u0,v0,y0,y2;
+    int u0,v0,y0,y2;
 
-    clock_t begin = clock();
+    double total_time = 0;
 
-    for (int i=0; i<360; i++) {
-        for (int j=0; j<320; j++) { //640/2 = process pixels in sets of 6
-            for (int k=0; k<2; k++) { //alternating pixels within macropixel sets of 3
-                u0 = buffer[counter] + buffer[counter+4]; //add the U values (u0 and u2)
-                v0 = buffer[counter+2] + buffer[counter+6]; //add the V values (v0 and v2)
+    for (int t = 0; t < 1000; t++)
+    {
+        counter = 0;
+        next_counter = 0;
+        index = 0;
 
-                //2 rows down
-                next_counter = counter + row_size*2;
-                u0 += buffer[next_counter] + buffer[next_counter+4]; //add the U values
-                v0 += buffer[next_counter+2] + buffer[next_counter+6]; //add the V values
+        clock_t begin = clock();
 
-                if (k == 0) {
-                    y0 = buffer[counter+1];
-                    y2 = buffer[counter+5];
-
-                    //2 rows down
-                    y0 += buffer[next_counter+1];
-                    y2 += buffer[next_counter+5];
-
-                    //goto the next macropixel
-                    counter += 4;
-                } else {
-                    y0 = buffer[counter+3];
-                    y2 = buffer[counter+7];
+        for (int i = 0; i < 360; i++)
+        {
+            for (int j = 0; j < 320; j++) //640/2 = process pixels in sets of 6
+            {                 
+                for (int k = 0; k < 2; k++) //alternating pixels within macropixel sets of 3
+                {
+                    u0 = buffer[counter] + buffer[counter + 4];     //add the U values (u0 and u2)
+                    v0 = buffer[counter + 2] + buffer[counter + 6]; //add the V values (v0 and v2)
 
                     //2 rows down
-                    y0 += buffer[next_counter+3];
-                    y2 += buffer[next_counter+7];
+                    next_counter = counter + row_size * 2;
+                    u0 += buffer[next_counter] + buffer[next_counter + 4];     //add the U values
+                    v0 += buffer[next_counter + 2] + buffer[next_counter + 6]; //add the V values
 
-                    //skip 8 bytes to get to the next set of 3 macropixels
-                    counter += 8;
+                    if (k == 0)
+                    {
+                        y0 = buffer[counter + 1];
+                        y2 = buffer[counter + 5];
+
+                        //2 rows down
+                        y0 += buffer[next_counter + 1];
+                        y2 += buffer[next_counter + 5];
+
+                        //goto the next macropixel
+                        counter += 4;
+                    }
+                    else
+                    {
+                        y0 = buffer[counter + 3];
+                        y2 = buffer[counter + 7];
+
+                        //2 rows down
+                        y0 += buffer[next_counter + 3];
+                        y2 += buffer[next_counter + 7];
+
+                        //skip 8 bytes to get to the next set of 3 macropixels
+                        counter += 8;
+                    }
+
+                    //compute totals and average
+                    u0 = u0 >> 2; //divide by 4 to get average
+                    v0 = v0 >> 2; //divide by 4 to get average
+                    y0 = (y0 + y2) >> 2; //divide by 4 to get average
+
+                    int avg_u = (u0);
+                    int avg_v = (v0);
+                    int avg_y = (y0);
+
+                    avg_y -= 16;
+                    avg_u -= 128;
+                    avg_v -= 128;
+
+                    float r = 1.164 * avg_y + 1.596 * avg_v;
+                    float g = 1.164 * avg_y - 0.392 * avg_u - 0.813 * avg_v;
+                    float b = 1.164 * avg_y + 2.017 * avg_u;
+
+                    if (b > 255)
+                        b = 255;
+                    else if (b < 0)
+                        b = 0;
+
+                    if (g > 255)
+                        g = 255;
+                    else if (g < 0)
+                        g = 0;
+
+                    if (r > 255)
+                        r = 255;
+                    else if (r < 0)
+                        r = 0;
+
+                    ba_new[index] = (unsigned char)b;
+                    index++;
+                    ba_new[index] = (unsigned char)g;
+                    index++;
+                    ba_new[index] = (unsigned char)r;
+                    index++;
                 }
-
-                //compute totals and average
-                float avg_u = (u0) / 4.0;
-                float avg_v = (v0) / 4.0;
-                float avg_y = (y0 + y2) / 4.0;
-
-                avg_y -= 16;
-                avg_u -= 128;
-                avg_v -= 128;
-
-                float r = 1.164 * avg_y                 + 1.596 * avg_v;
-                float g = 1.164 * avg_y - 0.392 * avg_u - 0.813 * avg_v;
-                float b = 1.164 * avg_y + 2.017 * avg_u;
-
-                if (b > 255)
-                    b = 255;
-                if (g > 255)
-                    g = 255;
-                if (r > 255)
-                    r = 255;
-
-                if (b < 0)
-                    b = 0;
-                if (g < 0)
-                    g = 0;
-                if (r < 0)
-                    r = 0;
-
-                ba_new[index]=(unsigned char)b;
-                index++;
-                ba_new[index]=(unsigned char)g;
-                index++;
-                ba_new[index]=(unsigned char)r;
-                index++;
             }
+            counter += row_size * 2;
+            //__builtin_prefetch (&buffer[counter + row_size*16]);
         }
-        counter += row_size*2;
-    }
 
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf ("time in ms:  %f\n", 1000.0 * (double)(end - begin) / CLOCKS_PER_SEC);
+        clock_t end = clock();
+        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+        total_time += time_spent;
+    }
+    printf ("time in ms:  %f\n", total_time);
 
     FILE *p = fopen("beach360.uyvy","wb");
     fwrite(ba_new,1,sizeof(ba_new),p);
