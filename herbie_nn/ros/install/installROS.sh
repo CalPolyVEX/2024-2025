78b9c1dee4ac8d82b17fb019a1f748264f8ec039
John Seng <jseng@calpoly.edu>
2021-06-24 16:06:17 -0700

    updated install scripts

--- herbie_nn/ros/install/installROS.sh
+++ herbie_nn/ros/install/installROS.sh
@@ -5,7 +5,7 @@
 # Setup sources.lst
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 # Installation
 sudo apt-get update
@@ -29,21 +29,21 @@ mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 git init
 git config core.sparseCheckout true
-echo 'herbie2/' > .git/info/sparse-checkout
+echo 'herbie_nn/' > .git/info/sparse-checkout
 git remote add -f origin https://github.com/jsseng/ue4.git
 git pull origin master
 git clone https://github.com/ros-perception/image_transport_plugins.git
+git clone https://github.com/ros-perception/image_common.git
 
 #vision_opencv
-git clone https://github.com/ros-perception/vision_opencv.git
+#git clone https://github.com/ros-perception/vision_opencv.git
 #For OpenCV4 compatibility
-#git clone https://github.com/BrutusTT/vision_opencv.git
-#git checkout 8e01b44c5c1c0003dc91273076f8ca7feb9a8025
+git clone https://github.com/BrutusTT/vision_opencv.git
+cd vision_opencv
+git checkout 8e01b44c5c1c0003dc91273076f8ca7feb9a8025
+cd ..
 
 git clone https://github.com/ros-drivers/video_stream_opencv.git
-git clone https://github.com/introlab/rtabmap_ros.git
-git clone https://github.com/ros-drivers/libuvc_ros.git
-git clone https://github.com/Slamtec/rplidar_ros.git
 
 #install dependencies
 sudo apt install python-pygame gfortran libogg-dev libtheora-dev
@@ -54,11 +54,6 @@ sudo apt install python-pip ros-melodic-pcl-conversions ros-melodic-pcl-ros ros-
 sudo apt install python3-pip ros-melodic-move-base-msgs ros-melodic-costmap-2d ros-melodic-octomap-msgs ros-melodic-find-object-2d ros-melodic-camera-info-manager
 sudo apt install ros-melodic-rviz ros-melodic-serial libuvc-dev ros-melodic-rqt-common-plugins
 sudo apt install ros-melodic-move-base ros-move-base-msgs ros-melodic-global-planner
-
-#install Tensorflow for python3 (Jetpack 4.2)
-#pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.13.1+nv19.4 --user
-#pip install numpy
-#pip3 install numpy
 
 #install vim pathogen
 sudo apt install curl
@@ -73,13 +68,8 @@ git config --global user.name "John Seng"
 #sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 #sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 
-#install g2o for rtabmap
-sudo apt install ros-melodic-libg2o
-
-if [ $1 -gt 100 ]  #if running on a Jetson board
+if [ $1 -gt 100 ]  #if running on a Jetson board use:  ./installROS.sh 200
 then
-   git clone https://github.com/stereolabs/zed-ros-wrapper.git
-
    #disable graphical boot
    systemctl set-default multi-user.target
 
--- herbie_nn/ros/install/installROS_noetic.sh
+++ herbie_nn/ros/install/installROS_noetic.sh
@@ -5,7 +5,7 @@
 # Setup sources.lst
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 # Installation
 sudo apt-get update
@@ -29,7 +29,7 @@ mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 git init
 git config core.sparseCheckout true
-echo 'herbie2/' > .git/info/sparse-checkout
+echo 'herbie_nn/' > .git/info/sparse-checkout
 git remote add -f origin https://github.com/jsseng/ue4.git
 git pull origin master
 git clone https://github.com/ros-perception/image_transport_plugins.git
@@ -41,9 +41,7 @@ git clone https://github.com/ros-perception/vision_opencv.git
 #git checkout 8e01b44c5c1c0003dc91273076f8ca7feb9a8025
 
 git clone https://github.com/ros-drivers/video_stream_opencv.git
-git clone https://github.com/introlab/rtabmap_ros.git
 git clone https://github.com/ros-drivers/libuvc_ros.git
-git clone https://github.com/Slamtec/rplidar_ros.git
 
 #install dependencies
 sudo apt install python-pygame gfortran libogg-dev libtheora-dev
@@ -73,13 +71,8 @@ git config --global user.name "John Seng"
 #sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 #sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 
-#install g2o for rtabmap
-sudo apt install ros-noetic-libg2o
-
 if [ $1 -gt 100 ]  #if running on a Jetson board
 then
-   git clone https://github.com/stereolabs/zed-ros-wrapper.git
-
    #disable graphical boot
    systemctl set-default multi-user.target
 
