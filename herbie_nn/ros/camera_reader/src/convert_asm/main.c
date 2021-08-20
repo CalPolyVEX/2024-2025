24e714abde4b9059e13a890caaaf09f4e0720e4d
John Seng <jseng@calpoly.edu>
2021-08-20 11:11:23 -0700

    working arm NEON conversion

--- herbie_nn/ros/camera_reader/src/convert_asm/main.c
+++ herbie_nn/ros/camera_reader/src/convert_asm/main.c
@@ -4,32 +4,41 @@ void convert_nhwc_asm(unsigned char* in, float* out);
 void convert_nhwc_asm_standard(unsigned char* in, float* out);
 
 int main() {
-   unsigned char a[100];
-   float b[100];
+   unsigned char a[640*360*3];
+   float b[640*360*3];
+   float c[640*360*3];
    
-   for (int i=0; i<100; i++) {
-      a[i] = i;
+   for (int i=0; i<640*360*3; i++) {
+      a[i] = i+1;
+      b[i] = 5;
+      c[i] = 10;
    }
 
    convert_nhwc_asm_standard(a, b);
-   for (int i=0; i<10; i++) {
+   for (int i=0; i<20; i++) {
       printf("%f ", b[i]);
    }
    printf("\n");
 
-   convert_nhwc_asm(a, b);
-   for (int i=0; i<10; i++) {
-      printf("%f ", b[i]);
+   convert_nhwc_asm(a, c);
+   for (int i=0; i<20; i++) {
+      printf("%f ", c[i]);
    }
    printf("\n");
+
+   for (int i=0; i<(640*360*3); i++) {
+      if ((c[i]-b[i]) > .000001) {
+         printf("error: %d\n", i);
+         break;
+      }
+   }
    
    return 0;
 }
 
-
 void convert_nhwc_asm_standard(unsigned char* src, float* dest) {
    //convert NHWC to NCHW with 3 channels
-   int num_pixels = 360 * 640;
+   int num_pixels = 640 * 360;
    unsigned char* temp_src;
    float div_255 = 1.00000 / 255.00000;
    
