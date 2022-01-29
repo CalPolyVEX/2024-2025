addc8b20039a623fdb2811537030ca9037c5b34a
Christian Honein <chonein@outlook.com>
2022-01-29 12:02:04 -0800

    added demo code for display

--- astromech/display/i2c_display/i2c_display.ino
+++ astromech/display/i2c_display/i2c_display.ino
@@ -61,14 +61,14 @@ hd44780_I2Cexp lcd; // declare lcd object: auto locate & auto config expander ch
 const int LCD_COLS = 20;
 const int LCD_ROWS = 4;
 
-uint8_t battery[8] = {
+byte battery[8] = {
     0b01110,
     0b11111,
     0b10001,
     0b10001,
     0b10001,
-    0b11111,
-    0b11111,
+    0b10001,
+    0b10001,
     0b11111};
 
 uint8_t rightarrow[8] = {
@@ -80,6 +80,36 @@ uint8_t rightarrow[8] = {
     0b00010,
     0b00100,
     0b01000};
+
+uint8_t leftarrow[8] = {
+    0b00010,
+    0b00100,
+    0b01000,
+    0b11111,
+    0b11111,
+    0b01000,
+    0b00100,
+    0b00010};
+
+uint8_t uparrow[8] = {
+    0b00100,
+    0b01110,
+    0b10101,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100};
+
+uint8_t downarrow[8] = {
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b00100,
+    0b10101,
+    0b01110,
+    0b00100};
 
 void setup() {
     int status;
@@ -111,27 +141,82 @@ void setup() {
 
 void loop() {
     // Creating char
+    static unsigned battery_lvl = 0;
     lcd.createChar(0, battery);
     lcd.createChar(1, rightarrow);
 
     lcd.print("Astromech");
-    lcd.setCursor(2, 2);
-    lcd.print('\001');
+    /* lcd.setCursor(2, 2);
+    lcd.print('\001'); */
+    switch (battery_lvl % 4) {
+    case 0:
+        right_arrow();
+        break;
 
+    case 1:
+        left_arrow();
+        break;
+
+    case 2:
+        up_arrow();
+        break;
+
+    case 3:
+        down_arrow();
+        break;
+
+    default:
+        break;
+    }
+
+    output_battery(battery_lvl, 10);
     delay(2000);
-    lcd.setCursor(2, 2);
-    lcd.print('\000');
     lcd.clear();
+    battery_lvl = (battery_lvl + 1) % 10;
+}
+
+void right_arrow() {
+    lcd.createChar(1, rightarrow);
+    lcd.setCursor(LCD_COLS - 1, 2);
+    lcd.print('\001');
+}
+
+void left_arrow() {
+    lcd.createChar(2, leftarrow);
+    lcd.setCursor(LCD_COLS - 1, 2);
+    lcd.print('\002');
+}
+
+void up_arrow() {
+    lcd.createChar(3, uparrow);
+    lcd.setCursor(LCD_COLS - 1, 2);
+    lcd.print('\003');
+}
+
+void down_arrow() {
+    lcd.createChar(4, downarrow);
+    lcd.setCursor(LCD_COLS - 1, 2);
+    lcd.print('\004');
 }
 
 /**
  * @brief Prints battery level in top right corner.
  * 
- * @param battery_level a number between 0 and 100. 
+ * 
+ * @param battery_level current voltage of battery.
+ * @param out_of voltage of battery when full. 
  */
-void output_battery(unsigned battery_level) {
-    if (battery_level > 100) {
-        battery_level = 100;
+void output_battery(unsigned battery_level, unsigned out_of) {
+    // battery level from 0 to 5
+    int i;
+    int out_of_5 = (double)battery_level / (double)out_of * 5.0;
+    for (int i = 6; i > 1; i--) {
+        battery[i] = 0b10001;
+    }
+    for (int i = 6; i > 6 - out_of_5; i--) {
+        battery[i] = 0b11111;
     }
     lcd.createChar(0, battery);
+    lcd.setCursor(LCD_COLS - 1, 0);
+    lcd.print('\000');
 }
