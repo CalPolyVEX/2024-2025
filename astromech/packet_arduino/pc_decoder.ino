d58d9d7ba5429ccffa4f595ce020825a2902557e
John Seng <jseng@jseng.csc.calpoly.edu>
2023-02-28 08:28:51 -0800

    if the packet size is large (255 bytes), the buffer would overflow

--- astromech/packet_arduino/pc_decoder.ino
+++ astromech/packet_arduino/pc_decoder.ino
@@ -44,7 +44,7 @@ const uint16_t crctable[256] =
         0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0};
 
 // The array of bytes from USB
-uint8_t bytes[256];
+uint8_t bytes[300];
 
 //////////////////////////////////////////////////
 // check_crc - check the crc of the incoming packet
