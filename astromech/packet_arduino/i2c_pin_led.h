e438b61d320b955567232605c6fa68ace45c3d13
Christian Honein <61818445+chonein@users.noreply.github.com>
2023-03-04 10:46:57 -0800

    added fix for Serial reference in TCA9534

--- astromech/packet_arduino/i2c_pin_led.h
+++ astromech/packet_arduino/i2c_pin_led.h
@@ -1,12 +1,13 @@
+// This is to fix the reference of Serial in TCA9534
+#define Serial SerialUSB
 #include <TCA9534.h>
+
 #include <Wire.h>
 
 #define LED1 3
 #define LED2 2
 #define LED3 1
 #define LED4 0
-
-#define Serial SerialUSB    // attempted fix for TCA9354 outdated Serial class
 
 void setup_i2c();
 void led_on(int num);
